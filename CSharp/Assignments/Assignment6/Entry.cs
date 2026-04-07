73224152695ba0954faaf01d284a54dd186122a5
Jayavardhini Muthukrishnan <jayavardhinim@infinite.com>
2026-04-07 12:25:31 +0530

    Assignment

--- CSharp/Assignments/Assignment6/Entry.cs
+++ CSharp/Assignments/Assignment6/Entry.cs
@@ -10,7 +10,9 @@ namespace Assignment6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2. Write a program in C# Sharp to create a file and write an array of strings to the file. Also Read from the file");
+            Console.WriteLine("1. Create a class called Books with BookName and AuthorName as members. Instantiate the class through constructor and also write a method Display() to display the details. \r\n\r\nCreate an Indexer of Books Object to store 5 books in a class called BookShelf. Using the indexer method assign values to the books and display the same.\r\n\r\nHint(use Aggregation/composition)");
+            Ques1.Bookindex();
+            Console.WriteLine("--------\n2. Write a program in C# Sharp to create a file and write an array of strings to the file. Also Read from the file");
             Ques2.StringArray();
             Console.WriteLine("--------\n3. Write a program in C# Sharp to count the number of lines in a file.");
             Ques3.Filelen();
---
+++ CSharp/Assignments/Assignment6/Ques1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public class Book
+    {
+        public string BookName;
+        public string AuthorName;
+
+        public Book(string bookname, string authorname)
+        {
+            BookName = bookname;
+            AuthorName = authorname;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Book Name: {BookName} \t Author Name: {AuthorName}");
+        }
+    }
+    public class BookShelf
+    {
+        private Book[] books = new Book[5];
+        public Book this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+
+        }
+        public void DisplayAll()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                books[i].display();
+            }
+        }
+    }
+    internal class Ques1
+    {
+        public static void Bookindex()
+        {
+            BookShelf shelf = new BookShelf();
+            shelf[0] = new Book("1984", "Abc");
+            shelf[1] = new Book("To kill a mockingbird", "efg");
+            shelf[2] = new Book("The alchemist", "hij");
+            shelf[3] = new Book("Life of pi", "klm");
+            shelf[4] = new Book("The kite runner", "nop");
+            shelf.DisplayAll();
+        }
+    }
+}
